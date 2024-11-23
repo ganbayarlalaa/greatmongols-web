d5b0253bc4460c5b8a6322131447e0fd1dffc264
Ganbayar <ganbayar@jacsys.mn>
2024-11-23 16:03:03 +0800

    add payment image and modal

---
+++ src/components/CustomModal.tsx
@@ -0,0 +1,28 @@
+import { Modal } from "flowbite-react";
+import { ReactNode } from "react";
+
+interface customModalInterface {
+  header?: string;
+  footer?: ReactNode;
+  body: ReactNode;
+  show: boolean;
+  onClose: () => void;
+}
+
+const CustomModal = ({
+  show,
+  onClose,
+  header,
+  body,
+  footer,
+}: customModalInterface) => {
+  return (
+    <Modal show={show} onClose={onClose}>
+      {header && <Modal.Header>{header}</Modal.Header>}
+      <Modal.Body>{body}</Modal.Body>
+      {footer && <Modal.Footer>{footer}</Modal.Footer>}
+    </Modal>
+  );
+};
+
+export default CustomModal;
