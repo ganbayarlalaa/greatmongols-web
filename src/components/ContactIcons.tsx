84c3ddc8a3dadd087d12cb3ab544fab577a6c55e
Ganbayar <ganbayar@jacsys.mn>
2024-11-23 18:07:03 +0800

    change contact data placement

--- src/components/ContactIcons.tsx
+++ src/components/ContactIcons.tsx
@@ -1,9 +1,4 @@
-const contact = {
-  instagram: "https://www.instagram.com/greatmongols_toastmasters_club/",
-  facebook: "https://www.facebook.com/GreatMongols",
-  gmail: "greatmongols.international@gmail.com",
-  phone: "99999999",
-};
+import { contact } from "../data/contact";
 
 export const Instagram = () => {
   return (
---
+++ src/data/contact.ts
@@ -0,0 +1,6 @@
+export const contact = {
+  instagram: "https://www.instagram.com/greatmongols_toastmasters_club/",
+  facebook: "https://www.facebook.com/GreatMongols",
+  gmail: "greatmongols.international@gmail.com",
+  phone: "99999999",
+};
