dc18e766914b9b01ed151b5f185ddd5bf137132b
ganbayarlalaa <ganbayarlalaa@gmail.com>
2024-11-24 18:17:14 +0800

    navigation fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,13 +9,13 @@ const Navigation = () => {
   ];
   return (
     <Navbar className="fixed z-50 w-full bg-white dark:bg-gray-800">
-      <Navbar.Brand>
+      <Navbar.Brand href="#home">
         <img
-          src="https://raw.githubusercontent.com/ganbayarlalaa/greatmongols-web/refs/heads/main/public/logo.png"
+          src="https://ganbayarlalaa.github.io/greatmongols-web/logo.png"
           className="mr-3 h-12 sm:h-14"
           alt="logo"
         />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        <span className="cursor-pointer self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           GREAT MONGOLS
         </span>
       </Navbar.Brand>
