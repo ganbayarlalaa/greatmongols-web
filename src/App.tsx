e79abff89d1be81e2d3951ef4344818bdddf1255
ganbayarlalaa <ganbayarlalaa@gmail.com>
2024-11-24 23:24:53 +0800

    change base url / for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 
 function App() {
-  return (
-<BrowserRouter basename="/greatmongols-web/">
-    <Routes>
-      <Route path="/" element={<HomePage />}/>
-    </Routes>
-</BrowserRouter>
-  );
+  return <HomePage />;
 }
 
 export default App;
